f95fe955f1627334a5c1023cad53d27ee1500fe9
Dostan <turlybekdostan@gmail.com>
2025-05-31 15:20:48 +0500

    Initial Commit

---
+++ NewsApplication/NewsApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApplicationApp.swift
+//  NewsApplication
+//
+//  Created by Dostan Turlybek on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
